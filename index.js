1cf821112040b2e607100700a771e862a6046f6a
kiendao <kiendao@var-meta.com>
2023-04-26 22:32:11 +0700

    update

--- index.js
+++ index.js
@@ -56,24 +56,37 @@ class AdsPortfolio {
     this.sku = sku;
     this.portfolio = portfolio;
   }
-} 
-class AdsT3{
-  constructor(portfolio,spend) {
+}
+class AdsT3 {
+  constructor(portfolio, spend) {
     this.portfolio = portfolio;
     this.spend = spend;
   }
 }
-class StorageFee{
-  constructor(fnsku,estimated_monthly_storage_fee) {
+class StorageFee {
+  constructor(fnsku, estimated_monthly_storage_fee) {
     this.fnsku = fnsku;
     this.estimated_monthly_storage_fee = estimated_monthly_storage_fee
+  }
+}
+class RemovalFee {
+  constructor(sku, order_type, removal_fee) {
+    this.sku = sku;
+    this.order_type = order_type;
+    this.removal_fee = removal_fee;
+  }
+}
+class SurchargeFee {
+  constructor(sku, short_time_range_long_term_storage_fee) {
+    this.sku = sku;
+    this.short_time_range_long_term_storage_fee = short_time_range_long_term_storage_fee
   }
 }
 class Result {
   constructor(sku, fnsku, sale_quantity, refund_quantity, product_sales, refund_amount, liquidations, gross_sales,
     product_sales_tax, shipping_credits, shipping_credit_tax, gift_wrap_credits, gift_wrap_credits_tax, regulatory_fee,
     regulatory_fee_tax, promotional_rebates, promotional_rebates_tax, marketplace_withheld_tax, referral_fees, fullfillment_fees, refund_commission, other_transaction_fee,
-    other_adjustment, gross_profits, ads, storage_fee) {
+    other_adjustment, gross_profits, ads, storage_fee, disposal_fee, aged_inventory_surcharge, gross_profits_overall) {
     this.sku = sku;
     this.fnsku = fnsku;
     this.sale_quantity = sale_quantity;
@@ -93,23 +106,26 @@ class Result {
     this.promotional_rebates_tax = promotional_rebates_tax;
     this.marketplace_withheld_tax = marketplace_withheld_tax;
     this.referral_fees = referral_fees;
-    this.fullfillment_fees= fullfillment_fees;
+    this.fullfillment_fees = fullfillment_fees;
     this.refund_commission = refund_commission;
     this.other_transaction_fee = other_transaction_fee;
-    this.other_adjustment= other_adjustment;
+    this.other_adjustment = other_adjustment;
     this.gross_profits = gross_profits;
     this.ads = ads;
     this.storage_fee = storage_fee;
+    this.disposal_fee = disposal_fee;
+    this.aged_inventory_surcharge = aged_inventory_surcharge;
+    this.gross_profits_overall = gross_profits_overall
   }
 }
 
-function getSKUData(paymentList, costOfGods, adsPortfolio, adsT3, storageFee) {
+function getSKUData(paymentList, costOfGods, adsPortfolio, adsT3, storageFee, removalFee, surChargeFee) {
   const skuData = {};
   paymentList.forEach(payment => {
     const { sku, type, quantity, product_sales, product_sales_tax, shipping_credits, shipping_credit_tax,
-    gift_wrap_credits , gift_wrap_credits_tax, regulatory_fee, regulatory_fee_tax, promotional_rebates,
-  promotional_rebates_tax, marketplace_withheld_tax, selling_fee, fba_fee, other_transaction_fee, other,
-total} = payment;
+      gift_wrap_credits, gift_wrap_credits_tax, regulatory_fee, regulatory_fee_tax, promotional_rebates,
+      promotional_rebates_tax, marketplace_withheld_tax, selling_fee, fba_fee, other_transaction_fee, other,
+      total } = payment;
     if (!skuData[sku]) {
       skuData[sku] = {
         sku,
@@ -123,19 +139,22 @@ total} = payment;
         shipping_credits: 0,
         shipping_credit_tax: 0,
         gift_wrap_credits: 0,
-        gift_wrap_credits_tax : 0,
-        regulatory_fee: 0 ,
+        gift_wrap_credits_tax: 0,
+        regulatory_fee: 0,
         regulatory_fee_tax: 0,
         promotional_rebates: 0,
         promotional_rebates_tax: 0,
         marketplace_withheld_tax: 0,
         referral_fees: 0,
-        fullfillment_fees:0,
-        refund_commission:0,
-        other_transaction_fee: 0 ,
+        fullfillment_fees: 0,
+        refund_commission: 0,
+        other_transaction_fee: 0,
         other: 0,
         gross_profits: 0,
-        storage_fee: 0
+        storage_fee: 0,
+        disposal_fee: 0,
+        aged_inventory_surcharge: 0,
+        gross_profits_overall: 0
       };
     }
     if (type === 'Order') {
@@ -160,7 +179,7 @@ total} = payment;
     skuData[sku].gift_wrap_credits += gift_wrap_credits;
     skuData[sku].gift_wrap_credits_tax += gift_wrap_credits_tax;
     skuData[sku].regulatory_fee += regulatory_fee;
-    skuData[sku].regulatory_fee_tax+= regulatory_fee_tax;
+    skuData[sku].regulatory_fee_tax += regulatory_fee_tax;
     skuData[sku].promotional_rebates += promotional_rebates;
     skuData[sku].promotional_rebates_tax += promotional_rebates_tax;
     skuData[sku].marketplace_withheld_tax += marketplace_withheld_tax;
@@ -174,25 +193,42 @@ total} = payment;
     const { sku, fnsku } = skuInfo;
     if (skuData[sku]) {
       skuData[sku].fnsku = fnsku;
-      storageFee.forEach(s=>{
-        if (fnsku === s.fnsku) {
-          skuData[sku].storage_fee += s.estimated_monthly_storage_fee;
-        } 
-      })
     }
   });
   adsPortfolio.forEach(ads => {
     const { sku, portfolio } = ads;
     if (skuData[sku]) {
       adsT3.forEach(ad => {
-        if (ad.portfolio === portfolio ) {
-          skuData[sku].ads = ad.spend
+        if (ad.portfolio === portfolio) {
+          skuData[sku].ads = parseFloat(ad.spend).toFixed(3) 
         }
       });
     }
   });
 
- 
+  removalFee.forEach(r => {
+    const { sku, removal_fee, order_type } = r;
+    if (skuData[sku]) {
+      if (order_type === 'Disposal' || order_type === 'Return') {
+        skuData[sku].disposal_fee += removal_fee
+      }
+    }
+  });
+
+  surChargeFee.forEach(s => {
+    const { sku, short_time_range_long_term_storage_fee } = s;
+    if (skuData[sku]) {
+      skuData[sku].aged_inventory_surcharge += short_time_range_long_term_storage_fee;
+    }
+  });
+  storageFee.forEach(s=>{
+    Object.values(skuData).forEach(v => {
+      if(s.fnsku === v.fnsku){
+        v.storage_fee += s.estimated_monthly_storage_fee;
+      }
+    });
+  })
+  
   return Object.values(skuData).map(sku => new Result(
     sku.sku,
     sku.fnsku,
@@ -218,8 +254,11 @@ total} = payment;
     sku.other_transaction_fee,
     sku.other,
     sku.gross_profits,
-    sku.ads,
-    sku.storage_fee
+    sku.ads ? Number(sku.ads): null,
+    sku.storage_fee,
+    sku.disposal_fee,
+    sku.aged_inventory_surcharge,
+    sku.gross_profits + sku.aged_inventory_surcharge
   ));
 }
 function GenerateFile() {
@@ -228,6 +267,8 @@ function GenerateFile() {
   const ws3 = workbook.Sheets["Ads Portfolio"]
   const ws4 = workbook.Sheets["Ads T3"]
   const ws5 = workbook.Sheets["Storage Fee T3"]
+  const ws6 = workbook.Sheets["Removal Fee T3"]
+  const ws7 = workbook.Sheets["Surcharge Fee T3"]
 
   // Use XLSX.utils.sheet_to_json() to convert the worksheet to a JSON array
   const jsonArray = XLSX.utils.sheet_to_json(worksheet);
@@ -291,23 +332,43 @@ function GenerateFile() {
   });
 
   const arr4 = XLSX.utils.sheet_to_json(ws4);
-  let adsT3 = arr4.map((row)=>{
+  let adsT3 = arr4.map((row) => {
     return new AdsT3(
       row['Portfolio'],
       row['Spend(USD)']
     )
   })
 
-  const arr5= XLSX.utils.sheet_to_json(ws5);
-  let storageFee = arr5.map((row)=>{
+  const arr5 = XLSX.utils.sheet_to_json(ws5);
+  let storageFee = arr5.map((row) => {
     return new StorageFee(
       row['fnsku'],
       row['estimated_monthly_storage_fee']
     )
   })
 
-  let rs = getSKUData(payments, costOfGods, adsPortfolio,adsT3, storageFee);
+  const arr6 = XLSX.utils.sheet_to_json(ws6)
+  let removalFee = arr6.map((row) => {
+    return new RemovalFee(
+      row['sku'],
+      row['order-type'],
+      row['removal-fee']
+    )
+  })
+
+  const arr7 = XLSX.utils.sheet_to_json(ws7)
+  let surChargeFee = arr7.map((row) => {
+    return new SurchargeFee(
+      row['sku'],
+      row['short-time-range-long-term-storage-fee']
+    )
+  })
+
+  let rs = getSKUData(payments, costOfGods, adsPortfolio, adsT3, storageFee, removalFee, surChargeFee);
   console.log(rs);
+  const newWorksheet = XLSX.utils.json_to_sheet(rs);
+  XLSX.utils.book_append_sheet(workbook, newWorksheet, "File hoan thanh");
+  XLSX.writeFile(workbook, 'example.xlsx');
 }
 
 GenerateFile()
